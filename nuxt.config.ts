798a968227054e43094dc82f006c74e58cd0441e
ML <me.mlaure@gmail.com>
2025-03-02 00:34:28 +0100

    exp: nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-06',
+  compatibilityDate: '2025-01-31',
 
   build: {
     transpile: ['shiki', 'ohash'],
